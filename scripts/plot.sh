ff258e76c8b92c4c9598f8d0d93f2c426f10aeb0
Anne van Rossum <anne@crownstone.rocks>
2017-02-26 02:16:51 +0100

    Implement getopt in plot.sh

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,7 +1,56 @@
 #!/bin/bash
 
-plot_type=${1:? "$0 plot-type data-file"}
-data_file=${2:? "$0 plot-type data-file"}
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+  echo "I’m sorry, `getopt --test` failed in this environment."
+  exit 1
+fi
+
+SHORT=vp:d:
+LONG=verbose,plot:,data:
+
+PARSED=`getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"`
+if [[ $? -ne 0 ]]; then
+  exit 2
+fi
+
+eval set -- "$PARSED"
+
+while true; do
+    case "$1" in
+      -v|--verbose)
+	verbose=y
+	shift
+	;;
+    -p|--plot)
+	plot_type=$2
+	shift 2
+        ;;
+    -d|--data) 
+	data_file=$2
+	echo $2
+	shift 2
+	;;
+    --)
+	shift
+	break
+	;;
+     *)
+	echo "Error in arguments."
+	echo $2
+	exit 3
+	;;
+    esac
+done
+
+usage() {
+  echo "Usage: $0 --plot plot_type --data data_file"
+}
+
+if [ ! "$plot_type" ] || [ ! "$data_file" ]; then
+  usage
+  exit 4
+fi
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 noparama_path="$script_path/.."
